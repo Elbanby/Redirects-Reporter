49dc58ac2ba41b693e892e3dceefe43583ecee83
omar Elbanby <omarelbanby@omars-iMac.local>
2018-09-14 22:52:40 -0400

    Add internet watcher template

---
+++ app/InternetWatcher.js
@@ -0,0 +1,13 @@
+/*
+  This class should create a internet watcher. Which will handle internet
+  failure and insure that the program still runs once the connectivity
+  is back.
+*/
+
+function test(){
+  console.log('I should watch for internet connectivity');
+}
+
+module.exports = {
+  test
+}
