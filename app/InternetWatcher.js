b72931c88ad64e240ea462b054a558610944a774
omar Elbanby <omarelbanby@omars-iMac.local>
2018-09-15 21:21:07 -0400

    Add the isOnline function and create an emitter (tbd how we will utalize it)

--- app/InternetWatcher.js
+++ app/InternetWatcher.js
@@ -4,10 +4,33 @@
   is back.
 */
 
-function test(){
-  console.log('I should watch for internet connectivity');
+const os = require('os');
+const events = require('events');
+var internetWatcher = new events.EventEmitter();
+
+internetWatcher.on('offline', function(msg){
+  //Instead of a message, do a callback that cleans up to where you stopped
+  console.log(msg);
+});
+
+internetWatcher.on('online', function(msg){
+  console.log(msg);
+});
+
+function isOnline() {
+  let network = os.networkInterfaces();
+  //Currently checking if the inrernet is working on ethernet 1
+  if (network.en1) {
+    internetWatcher.emit('online','We are online');
+    return true;
+  } else {
+    internetWatcher.emit('offline','No internet connetion');
+    return false;
+  }
 }
 
+
+
 module.exports = {
-  test
+  isOnline
 }
