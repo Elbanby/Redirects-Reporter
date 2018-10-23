bfa40a74de3aa7a6f85351772741c7e1339f323c
Omar Elbanby <omar.el-banby2@gwl.ca>
2018-10-23 13:05:34 -0400

    Enable tracing the redirect link and finding the status code

--- index.js
+++ index.js
@@ -21,17 +21,20 @@ const rl = readline.createInterface(inStream, outStream);
 
 const username = cridentials.username;
 const password = cridentials.password;
-const host = "www.greatwestlife.com";
+
 const auth =
   "Basic " + new Buffer(`${username}:${password}`).toString("base64");
 
-const fileHeaders = `Original Url, Status Code, Final Url, Status\n`
+const fileHeaders = `Original Url, Status Code, Final Url, Status\n`;
 
 var urlObjArray = [];
 var errorObjArray = [];
 
-var chunk = 300;
+//// TODO: dynamically decide the number of chunk
+var chunk = 200;
 var index = 0;
+
+var numberOfUrls = 0;
 
 var startIndex = 0;
 
@@ -44,33 +47,37 @@ let startTime = new Date().toLocaleTimeString();
 append(resultPath + outputFile, fileHeaders);
 
 console.log("Program start : " + startTime);
-rl.on("line", path => {
+rl.on("line", line => {
+  numberOfUrls++;
+  //line = encodeURI(line);
+  const path = line.substring(line.indexOf(".com/") + 5);
   let tempObj = {
     path: path,
-    originalPath: '',
+    numFailed: -1,
+    originalPath: "",
     index: -1,
     statusCode: -1,
     finialStausCode: -1,
     finalUrl: "",
-    redirected: false
+    redirected: false,
+    host: ""
   };
   tempObj.index = index++;
   tempObj.originalPath = path;
   tempObj.path = path;
+  tempObj.host = line.substring("https://".length, line.indexOf(".com/") + 4);
   urlObjArray.push(tempObj);
 });
 
 rl.on("close", () => {
+  chunk = (numberOfUrls > 200)? 200 : numberOfUrls;
   propagateRequests(startIndex, chunk);
 });
 
 function propagateRequests(startIndex, chunk) {
-
   if (sentRequest == urlObjArray.length) {
-    let endTime = new Date().toLocaleTimeString();
-    console.log(`${startTime} ..... ${endTime}`);
+    return;
   }
-
   let subQueue = urlObjArray.slice(startIndex, chunk);
 
   subQueue.forEach(obj => {
@@ -87,28 +94,37 @@ function propagateRequests(startIndex, chunk) {
         obj.finialStausCode = res.statusCode;
 
         if (res.statusCode != 301 && res.statusCode != 307) {
-          let str = ''
+          let str = "";
           if (obj.redirected) {
-            str = `${host}/${obj.originalPath}, ${obj.statusCode}, ${host}/${obj.path}, ${obj.finialStausCode}\n`
+            //// TODO: if 200 make sure you display 200 as well
+            str = `${obj.host}/${obj.originalPath}, ${obj.statusCode}, ${
+              obj.host
+            }/${obj.path}, ${obj.finialStausCode}\n`;
           } else {
-            str = `${host}/${obj.path}, ${res.statusCode}, ${res.headers.location}, ${res.statusCode}\n`;
+            str = `${obj.host}/${obj.path}, ${
+              res.statusCode
+            }, 'no redirection', ${res.statusCode}\n`;
           }
-           append(resultPath + outputFile, str);
+          append(resultPath + outputFile, str);
         } else {
-          console.log('🧐 trace redirects');
+          console.log("🧐 trace redirects");
           //Now you need to change the properties in the urlObj then put it back
           //In the end of the que.
-          obj.path = getPath(res.headers.location);
+          obj.path = getPath(obj.host, res.headers.location);
           obj.statusCode = res.statusCode;
           obj.finialStausCode = -1;
           obj.redirected = true;
           urlObjArray.push(obj);
         }
         singleRequest();
+        return;
       })
       .catch(err => {
-        let str = `${host}/${obj.path}, ${err.code} \n`;
-        if (err.code === "ETIMEDOUT" || err.code === "ECONNRESET") {
+        let str = `${obj.host}/${obj.path}, ${err.code} \n`;
+        if (
+          (err.code === "ETIMEDOUT" || err.code === "ECONNRESET") &&
+          obj.numFailed < 2
+        ) {
           console.log(
             obj.path +
               ">>>>>TIME OUT || Socket hang up >> Added to the end of the queue " +
@@ -117,11 +133,18 @@ function propagateRequests(startIndex, chunk) {
           urlObjArray.push(obj);
           singleRequest();
         } else {
-          let str =  `${obj.path}, ${err.code}\n`
+          let str = `https://${obj.host}${obj.path}, ${err.code}\n`;
           append(resultPath + errorFile, str);
           console.log(
-            "\nERROR CODE 02 " + "REQUEST RETURNED : " + returnedRequests + ' ' + err
+            "\nERROR CODE 02 " +
+              "REQUEST RETURNED : " +
+              returnedRequests +
+              " " +
+              err +
+              "URL: " +
+              str
           );
+          return;
         }
       });
   });
@@ -132,8 +155,8 @@ function get(urlObj) {
   console.log(`Number of requests sent: ------------> ${sentRequest}`);
   const options = {
     method: "GET",
-    host: host,
-    path: encodeURI("/" + urlObj.path),
+    host: urlObj.host,
+    path: "/" + urlObj.path,
     timeout: 30000,
     followAllRedirects: true,
     headers: {
@@ -144,12 +167,11 @@ function get(urlObj) {
     https
       .get(options, res => {
         returnedRequests++;
-        res.on('data', (data) => {
-          resolve(res);
-        });
+        resolve(res);
       })
       .on("error", err => {
         returnedRequests++;
+        urlObj.numFailed++;
         reject(err);
       });
   });
@@ -164,12 +186,19 @@ function append(file, str) {
 }
 
 function singleRequest() {
-  startIndex = chunk;
-  chunk += 1;
-  propagateRequests(startIndex, chunk);
+  if (sentRequest != urlObjArray.length) {
+    startIndex = chunk;
+    chunk += 1;
+    propagateRequests(startIndex, chunk);
+  } else {
+    console.log("ending");
+    let endTime = new Date().toLocaleTimeString();
+    console.log(`${startTime} ..... ${endTime}`);
+    return;
+  }
 }
 
-function getPath(url) {
+function getPath(host, url) {
   let strLen = `https://${host}/`.length;
   return url.substring(strLen);
 }
